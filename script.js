8faa8f81b63e9509bf2b603b393f81aec8f084f0
Chris kabue <chris.kabue@student.moringaschool.com>
2025-09-28 13:25:32 +0300

    Initial project files

---
+++ script.js
@@ -0,0 +1,34 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(product, price) {
+  cart.push({ product, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(product + " added to cart!");
+}
+
+function displayCart() {
+  let cartItems = document.getElementById("cart-items");
+  let total = document.getElementById("total");
+  cartItems.innerHTML = "";
+  let sum = 0;
+
+  cart.forEach((item, index) => {
+    cartItems.innerHTML += `<p>${item.product} - $${item.price} 
+    <button onclick="removeItem(${index})">Remove</button></p>`;
+    sum += item.price;
+  });
+
+  total.innerText = "Total: $" + sum;
+}
+
+function removeItem(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
+window.onload = function() {
+  if (document.getElementById("cart-items")) {
+    displayCart();
+  }
+};
